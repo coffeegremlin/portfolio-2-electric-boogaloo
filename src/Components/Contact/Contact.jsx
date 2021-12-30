419ad52ee125d549cb82ef419640585ef62c3b1e
coffeegremlin <natemausert@gmail.com>
2021-12-30 15:48:24 -0500

    added font-awesome svg icons in assets.

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Tweet } from "react-twitter-widgets";
 
 // Assets
 import Background from '../../Assets/background.jpg'
@@ -11,6 +12,7 @@ const Contact = () => {
         <h3>Drop a line sometime! Here's some ways to reach out to me. Thanks for stopping by!</h3>
         {/* Add image with links to my personal contact information */}
         {/* use fontawesome icons */}
+        <Tweet tweetId='1471669858727342086' />
       </div>
     </>
   )
