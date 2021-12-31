a80ffb14fd3b93306cde453d79a0314bebdaa2bb
coffeegremlin <natemausert@gmail.com>
2021-12-31 16:54:37 -0500

    added overflow scroll, resized background to cover overflow, added links on svg icons in conact.jsx, need to add cards to projects.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,18 +1,48 @@
 import React from "react";
-import { Tweet } from "react-twitter-widgets";
+import { Timeline, Tweet } from "react-twitter-widgets";
 
 // Assets
 import Background from '../../Assets/background.jpg'
+import GithubSquare from '../../Assets/font-awesome/github-brands.svg'
+import Mail from '../../Assets/font-awesome/envelope-regular.svg'
+import LinkedIn from '../../Assets/font-awesome/linkedin-brands.svg'
+
 
 const Contact = () => {
   return (
     <>
       <div className='background-img' style={{ backgroundImage: `url(${Background})`}}>
-        <h1>Contact:</h1>
-        <h3>Drop a line sometime! Here's some ways to reach out to me. Thanks for stopping by!</h3>
-        {/* Add image with links to my personal contact information */}
-        {/* use fontawesome icons */}
-        <Tweet className='tweet' tweetId='1471669858727342086' />
+        <div className='contact'>
+          <h1>Contact:</h1>
+          <h3>Drop a line sometime! Here's some ways to reach out to me. Thanks for stopping by!</h3>
+          {/* Add image with links to my personal contact information */}
+          {/* use fontawesome icons */}
+        </div>
+        <div className='contact-links'>
+          <h3>My links!</h3>
+            <a href="https://github.com/coffeegremlin">
+              <img src={GithubSquare} alt="github" />
+            </a>
+            <a href="mailto:natemausert@gmail.com">
+              <img src={Mail} alt='My email'/>
+            </a>
+            <a href="https://www.linkedin.com/in/nathanmausert/">
+              <img src={LinkedIn} alt='My Linkedin' />
+            </a>
+        </div>
+          <div className="tweets">
+            <Timeline 
+              className='tweets'
+              dataSource={{
+                sourceType: 'profile',
+                screenName: 'NMausert',
+              }}
+              options={{
+                height: '400',
+                width: '350',
+              }}
+            />
+          </div>
       </div>
     </>
   )
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,6 +7,7 @@ const Home = (props) => {
   return (
     <>
       <div className='background-img' style={{ backgroundImage: `url(${Background})`}}>
+        {/* add in my face in a round border, pixel, or neon frame. */}
         <h1 id='splash-logo'>Welcome to my portfolio site! </h1>
         <p>I'm Nathan Mausert, and I'm a full-stack software engineer with a focus on web development.</p>
       </div>
