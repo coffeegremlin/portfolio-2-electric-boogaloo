dc699eeb729ed4a8fdb63cf24ab7d022418d452f
coffeegremlin <natemausert@gmail.com>
2022-01-01 01:11:26 -0500

    fiddling with skills and contact

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -8,7 +8,7 @@ const Skills = () => {
   return (
     <>
       <div className='background-img' style={{backgroundImage: `url(${Background})`}}>
-        {/* <img src='https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white'/> */}
+        {/* <img src={https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white} /> */}
         {/* add in the img badges for individiual skills either through url or add them to assets later on */}
       </div>
     </>
