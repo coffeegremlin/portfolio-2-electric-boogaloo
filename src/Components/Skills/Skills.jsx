0259c6d78fc3eee07520ccd72f7c331725fbec3c
coffeegremlin <natemausert@gmail.com>
2022-01-02 14:11:04 -0500

    fixed skills page

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -8,40 +8,18 @@ const Skills = () => {
   return (
     <>
       <div className='background-img' style={{backgroundImage: `url(${Background})`}}>
-        <h1 id='skillstitle'>My Skills, Stacks, and frameworks</h1>
         <div className='skills'>
-          <div id='languages'>
+        <h1 id='skillstitle'>Skills and Tech</h1>
+
+          <div className='neon-wrapper' id='languages'>
             <h3>Languages</h3>
-            <table>
-              <p>HTML</p>
-              <p>CSS</p>
-              <p>javaScript</p>
-              <p>Python</p>
-            </table>
-          </div>
-          <div className='skills' id='frameworks'>
+            <p>HTML, CSS, javaScript, Python</p>
             <h3>Frameworks/Stacks</h3>
-            <table>
-              <p>Django</p>
-              <p>React</p>
-              <p>MEN and MERN stacks</p>
-            </table>
-          </div>
-          <div className='skills' id='otherTech'>
+            <p>Django, React, MEN and MERN stacks</p>
             <h3>Other Shenanigans and technologies</h3>
-            <table>
-              <p>MongoDB</p>
-              <p>Express.JS</p>
-              <p>Node.JS</p>
-              <p>PostgreSQL</p>
-              <p>Git</p>
-              <p>GitHub</p>
-              <p>BootStrap</p>
-              <p>API wrangler Extroidinare</p>
-            </table>
+            <p>MongoDB, Express.JS, Node.JS, PostgreSQL, Git, GitHub, BootStrap, API wrangling</p>
           </div>
         </div>
-        {/* add in the img badges for individiual skills either through url or add them to assets later on */}
       </div>
     </>
   )
