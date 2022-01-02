7bc10c57a8e3116ee75e3d6edcdd7d52670b60f0
coffeegremlin <natemausert@gmail.com>
2022-01-02 13:20:03 -0500

    added padding to bottom of about to fix postioning of text

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   return(
     <>
       <div className='background-img' style={{ backgroundImage: `url(${Background})`}}>
-        <div className="about-info">
+        <div className='about-info'>
           <h1>About Me:</h1>
           <p> I'm based in New York, with a planned move to Los Angeles soon. I have an interest in projects that improve users' lives. I aim to solve problems on both interpersonal and logistical levels and I'm driven by a passion for providing tangible, meaningful results I can be proud of. I aim to offer well thought out and expertly crafted solutions to complex problems with intention and strong communication skills.</p>
         </div>
